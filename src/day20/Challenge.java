94d3e6c93dabce0f5fd4189411a412a17d1b7c1d
akshat <akshat.tandon@tum.de>
2023-12-21 15:05:11 +0100

    Boilerplate code for rest of the days

---
+++ src/day20/Challenge.java
@@ -0,0 +1,17 @@
+package day20;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Challenge {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("src/resources/day20Input.txt"));
+        String input;
+
+        while (scanner.hasNextLine()) {
+            input = scanner.nextLine();
+        }
+
+    }
+}
